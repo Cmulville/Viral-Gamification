d4b0e87e32cf2455992b3a92805bce6b33319928
jackieooikk <98008869+jackieooikk@users.noreply.github.com>
2022-08-30 21:39:48 +1000

    Writing out the register screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,103 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../assets/images/logo.png")}
+      />
+
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text></Text>
+        <Text style={styles.forgot_button}>Don't have an account? Sign up</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#7bb2be",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    marginBottom: 40,
+    height: 300,
+    width: 300,
+  },
+
+  inputView: {
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "left",
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    width: "100%",
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FFF",
+  },
+});
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native-web";
+
+function RegisterScreen() {
+  //inputText variables
+  return <View styles={styles.Container}></View>;
+}
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+  },
+});
