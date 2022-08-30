118a6c1b6fbb02e758a83d0b9fb7b1e7b366051e
K0zzas17 <ochenje@gmail.com>
2022-08-30 12:00:38 +1000

    added map function

--- navigation/screens/mapScreen.js
+++ navigation/screens/mapScreen.js
@@ -1,12 +1,88 @@
 import * as React from 'react';
-import { View, Text } from 'react-native'
+import MapView, { Callout, Circle, Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from "expo-location"
 
-export default function MapScreen({navigation}) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold'}}>Map Screen</Text>
-        </View>
-    )
-} 
+export default function MapScreen() {
+  // constant that stores a pin and method (setpin) that changes it. values are just dummy data
+  const [pin, setPin] = React.useState({latitude: -27.470125,
+                                        longitude: 153.021072,});
+  // event that get asks for permission then gets the users inital location
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      } else {
+        console.log('Permission to access locaion was approved')
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+
+      // set Pin being used to change the pin
+      setPin({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+      });
+    })();
+    }, []);
+ 
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map}
+              // where the map will hover when opened Location is brisbane CBD
+              initialRegion = {{latitude: -27.470125,
+                                longitude: 153.021072,
+                                latitudeDelta: 0.5,
+                                longitudeDelta: 0.5}}
+                showsUserLocation = {true}
+                //method that will update the location of user when it changes
+                onUserLocationChange = {(e) => {
+                  console.log("location changed", e.nativeEvent.coordinate);
+                  setPin({
+                    latitude: e.nativeEvent.coordinate.latitude,
+                    longitude: e.nativeEvent.coordinate.longitude,
+                });
+                }}
+                >
+          <Marker 
+            // marker that shows Birsbane and says Brisbane when hovered
+            coordinate = {{latitude: -27.470125,
+                          longitude: 153.021072,}}
+            pinColor = "blue"
+            >
+                <Callout>
+						      <Text>Brisbane</Text>
+					      </Callout>
+          </Marker>
+          <Marker
+            // marker that shows the user location and is on top of the user icon from the MapView 
+            coordinate = {pin}
+            pinColor = "red"
+            >
+              <Callout>
+						    <Text>Marker linked to User Location</Text>
+					    </Callout>
+          </Marker>
+        
+          <Circle //circle that is around the user, maybe can be used as the infection radius
+          center={pin} radius={100}/>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
