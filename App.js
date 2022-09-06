ac48af94089217b75112454ebf10b4e245b6cc4a
NemanjaGojkovic1 <111219892+NemanjaGojkovic1@users.noreply.github.com>
2022-09-06 20:03:51 +1000

    copied na-bar, map testing

--- App.js
+++ App.js
@@ -1,88 +1,10 @@
 import * as React from 'react';
-import MapView, { Callout, Circle, Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import * as Location from "expo-location"
+import Main from './navigation/Main';
 
-export default function App() {
-  // constant that stores a pin and method (setpin) that changes it. values are just dummy data
-  const [pin, setPin] = React.useState({latitude: -27.470125,
-                                        longitude: 153.021072,});
-  // event that get asks for permission then gets the users inital location
-  React.useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
-        return;
-      } else {
-        console.log('Permission to access locaion was approved')
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      console.log(location);
-
-      // set Pin being used to change the pin
-      setPin({
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-      });
-    })();
-    }, []);
- 
+function App() {
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map}
-              // where the map will hover when opened Location is brisbane CBD
-              initialRegion = {{latitude: -27.470125,
-                                longitude: 153.021072,
-                                latitudeDelta: 0.5,
-                                longitudeDelta: 0.5}}
-                showsUserLocation = {true}
-                //method that will update the location of user when it changes
-                onUserLocationChange = {(e) => {
-                  console.log("location changed", e.nativeEvent.coordinate);
-                  setPin({
-                    latitude: e.nativeEvent.coordinate.latitude,
-                    longitude: e.nativeEvent.coordinate.longitude,
-                });
-                }}
-                >
-          <Marker 
-            // marker that shows Birsbane and says Brisbane when hovered
-            coordinate = {{latitude: -27.470125,
-                          longitude: 153.021072,}}
-            pinColor = "blue"
-            >
-                <Callout>
-						      <Text>Brisbane</Text>
-					      </Callout>
-          </Marker>
-          <Marker
-            // marker that shows the user location and is on top of the user icon from the MapView 
-            coordinate = {pin}
-            pinColor = "red"
-            >
-              <Callout>
-						    <Text>Marker linked to User Location</Text>
-					    </Callout>
-          </Marker>
-        
-          <Circle //circle that is around the user, maybe can be used as the infection radius
-          center={pin} radius={100}/>
-      </MapView>
-    </View>
+    <Main/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+export default App;
---
+++ navigation/screens/loginScreen.js
@@ -0,0 +1,101 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+export default function LoginScreen({navigation}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+    <Text
+          onPress={() => navigation.navigate('Home')}
+          style={{ fontSize: 26, fontWeight: 'bold'}}>Login</Text>
+        
+ 
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+      
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text></Text>
+        <Text style={styles.forgot_button}>Don't have an account? Sign up</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7bb2be",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  // image: {
+  //   marginBottom: 40,
+  //   height: 300,
+  //   width: 300,
+  // },
+
+  inputView: {
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "left",
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FFF",
+  },
+});
