354b205d3c98587ccb056368126320dd009c9529
K0zzas17 <ochenje@gmail.com>
2022-09-07 10:30:13 +1000

    Mainscreen error

--- navigation/MainScreen.js
+++ navigation/MainScreen.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from '@react-navigation/stack';
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 //Screens
@@ -9,43 +10,80 @@ import HomeScreen from "./screens/homeScreen";
 import SettingsScreen from "./screens/settingsScreen";
 import MapScreen from "./screens/mapScreen";
 import LoginScreen from "./screens/loginScreen";
-import DetailScreen from "./screens/detailScreen";
+import InventoryScreen from "./screens/inventoryScreen";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 // Screen names
-const settingsName = "Inventory";
+const inventoryName = "Inventory";
 const mapName = "Map";
 const friendList = "Friends";
 
+// Player status
+  const status = 'Immune'
+  const points = 0
+const statusColours = {
+  'Cured': '#05cf02',
+  'Infected' : '#f52718',
+  'Immune' : '#0aefff'
+}
+const screenColors = statusColours[status]
+
 const Tab = createBottomTabNavigator();
 const topTab = createMaterialTopTabNavigator();
+const Stack = createStackNavigator();
 
 export default function MainScreen({ navigation }) {
   return (
-    <Tab.Navigator
-      initalRouteName={MainScreen}
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          let rn = route.name;
+      <Tab.Navigator
+        initalRouteName={MainScreen}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
-          if (rn === mapName) {
-            iconName = focused ? "navigate" : "navigate-outline";
-          } else if (rn === settingsName) {
-            iconName = focused ? "briefcase" : "briefcase-outline";
-          } else if (rn == friendList) {
-            iconName = focused ? "people" : "people-outline";
-          }
+            if (rn === mapName) {
+              iconName = focused ? "navigate" : "navigate-outline";
+            } else if (rn === inventoryName) {
+              iconName = focused ? "briefcase" : "briefcase-outline";
+            } else if (rn == friendList) {
+              iconName = focused ? "people" : "people-outline";
+            }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
+            return <Ionicons name={iconName} size={size} color={screenColors}/>;
+          },
+          headerStyle: {
+                        backgroundColor: screenColors,
+                        height: 100,
+  
+                      //   alignItems: 'center'
         },
-      })}
-    >
-      <Tab.Screen name={mapName} component={MapScreen} />
-      <Tab.Screen name={settingsName} component={SettingsScreen} />
-      <Tab.Screen name={friendList} component={SettingsScreen} />
+            // headerRight: () => (
+            //   <Button
+            //     onPress={() => navigation.navigate(loginName)}
+            //     title="Login"
+            //     color="#0"
+            //   />
+            // ),
+            
+            title: status+" | Points:"+" "+points
+          
+        })}
+      >
+        <Tab.Screen name={mapName} component={MapScreen}
+        options={{ 
+                      tabBarLabel: mapName,
+                      
+                      }}/>
+        <Tab.Screen name={inventoryName} component={InventoryScreen}
+        options={{ 
+                      tabBarLabel: inventoryName
+                      }}/>
+        <Tab.Screen name={friendList} component={SettingsScreen} 
+        options={{ 
+                      tabBarLabel: friendList
+                      }}/>
 
-      {/* <Tab.Screen name={detailName} component={DetailScreen}/> */}
-    </Tab.Navigator>
+        {/* <Tab.Screen name={detailName} component={DetailScreen}/> */}
+      </Tab.Navigator>
   );
 }
--- navigation/RootStackScreen.js
+++ navigation/RootStackScreen.js
@@ -7,10 +7,20 @@ import MainScreen from "./MainScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 
 const RootStack = createStackNavigator();
+const status = 'Immune'
+const points = 0
+const statusColours = {
+  'Cured': '#05cf02',
+  'Infected' : '#f52718',
+  'Immune' : '#0aefff'
+}
+const screenColors = statusColours[status]
 
 const RootStackScreen = ({ navigation }) => {
   return (
-    <RootStack.Navigator headerMode="false">
+    <RootStack.Navigator 
+    screenOptions
+    >
       <RootStack.Screen name="LoginScreen" component={LoginScreen} />
       <RootStack.Screen name="MainScreen" component={MainScreen} />
       <RootStack.Screen name="RegisterScreen" component={RegisterScreen} />
