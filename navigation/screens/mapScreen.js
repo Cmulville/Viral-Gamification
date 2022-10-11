54b8eb6dc735e9817175c6ced5c6de296c52fad7
jackieooikk <98008869+jackieooikk@users.noreply.github.com>
2022-10-11 12:45:40 +1000

    items now spawn near your current location

--- navigation/screens/mapScreen.js
+++ navigation/screens/mapScreen.js
@@ -1,29 +1,44 @@
 import * as React from "react";
 import MapView, { Callout, Circle, Marker } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions, Image, icon, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  icon,
+  Alert,
+} from "react-native";
 import * as Location from "expo-location";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Axios from "axios";
 import { randomCirclePoint } from "random-location";
 
 export default function MapScreen() {
+  const UNIQUEITEMS = 3;
   // constant that stores a pin and method (setpin) that changes it. values are just dummy data
-  const birsbane = {latitude: -27.4705,
-                    longitude: 153.0260};
-  const uq = {latitude: -27.4975,
-              longitude: 153.0137};
-  const [pin, setPin] = React.useState({latitude: -27.470125,
-                                        longitude: 153.021072,});
-  const [distance, setDistance] = React.useState({thing: 0});
+  const birsbane = { latitude: -27.4705, longitude: 153.026 };
+  const uq = { latitude: -27.4975, longitude: 153.0137 };
+  const [pin, setPin] = React.useState({
+    latitude: -27.470125,
+    longitude: 153.021072,
+  });
+  const [distance, setDistance] = React.useState({ thing: 0 });
   const [user, setUser] = React.useState("");
-  const [testuser, setTestUser] = React.useState({latitude: 0, 
-                                                  latitude: 0});
-  const [items, setItems] = React.useState({ids: [],
-                                            idtypes: [],
-                                            latitude: [], 
-                                            latitude: []});
+  const [testuser, setTestUser] = React.useState({ latitude: 0, latitude: 0 });
+  // const [items, setItems] = React.useState({
+  //   ids: [],
+  //   idtypes: [],
+  //   latitude: [],
+  //   latitude: [],
+  // });
   const [friends, setFriends] = React.useState([]);
   const [count, setCount] = React.useState(0);
+  const [items, setItems] = React.useState([]);
+  const [initialCentre, setInitialCentre] = React.useState({
+    latitude: 0,
+    longitude: 0,
+  });
 
   const getUser = async () => {
     try {
@@ -47,8 +62,8 @@ export default function MapScreen() {
         username: user,
       }
     ).then((response) => {
-        setFriends(response.data.friends);
-        console.log(friends);
+      setFriends(response.data.friends);
+      console.log(friends);
     });
   };
 
@@ -62,16 +77,19 @@ export default function MapScreen() {
     //birsbane is lat 1
     //uq lat 2
     let R = 6371000; // metres
-    let phi1 = lat1 * Math.PI/180; // φ, λ in radians
-    let phi2 = lat2 * Math.PI/180;
-    let deltaphi = (lat2 - lat1) * Math.PI/180;
-    let deltalambda = (long2 - long1) * Math.PI/180;
-    let a = Math.sin(deltaphi/2) * Math.sin(deltaphi/2) +
-              Math.cos(phi1) * Math.cos(phi2) *
-              Math.sin(deltalambda/2) * Math.sin(deltalambda/2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    let phi1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+    let phi2 = (lat2 * Math.PI) / 180;
+    let deltaphi = ((lat2 - lat1) * Math.PI) / 180;
+    let deltalambda = ((long2 - long1) * Math.PI) / 180;
+    let a =
+      Math.sin(deltaphi / 2) * Math.sin(deltaphi / 2) +
+      Math.cos(phi1) *
+        Math.cos(phi2) *
+        Math.sin(deltalambda / 2) *
+        Math.sin(deltalambda / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     let d = R * c; // in metres
-    return(d);
+    return d;
   };
 
   const errorAlert = () => {
@@ -90,11 +108,14 @@ export default function MapScreen() {
 
   const UpdateLocation = () => {
     getUser();
-    Axios.post("https://deco3801-betterlatethannever.uqcloud.net/location/update", {
-      user: user,
-      latitude: pin.latitude,
-      longitude: pin.longitude,
-    }).then((response) => {
+    Axios.post(
+      "https://deco3801-betterlatethannever.uqcloud.net/location/update",
+      {
+        user: user,
+        latitude: pin.latitude,
+        longitude: pin.longitude,
+      }
+    ).then((response) => {
       if (response.data.message) {
         errorAlert();
       } else {
@@ -105,34 +126,45 @@ export default function MapScreen() {
 
   const GetLocation = () => {
     getUser();
-    Axios.post("https://deco3801-betterlatethannever.uqcloud.net/location/get", {
-      user: "Test@gamil"
-    }).then((response) => {
+    Axios.post(
+      "https://deco3801-betterlatethannever.uqcloud.net/location/get",
+      {
+        user: "Test@gamil",
+      }
+    ).then((response) => {
       if (response) {
-        setTestUser({latitude: response.data.loc[1],
-                    longitude: response.data.loc[0]});
-      } 
+        setTestUser({
+          latitude: response.data.loc[1],
+          longitude: response.data.loc[0],
+        });
+      }
     });
   };
 
   const getItems = () => {
-    Axios.post("https://deco3801-betterlatethannever.uqcloud.net/items/get", {
-    }).then((response) => {
+    Axios.post(
+      "https://deco3801-betterlatethannever.uqcloud.net/items/get",
+      {}
+    ).then((response) => {
       if (response) {
-        setItems({ids: response.data.loc[0],
-                  itemType: response.data.loc[1],
-                  latitude: response.data.loc[3],
-                  longitude: response.data.loc[2]});
-      } 
+        setItems({
+          ids: response.data.loc[0],
+          itemType: response.data.loc[1],
+          latitude: response.data.loc[3],
+          longitude: response.data.loc[2],
+        });
+      }
     });
   };
 
   const countItems = () => {
-    Axios.post("https://deco3801-betterlatethannever.uqcloud.net/items/count", {
-    }).then((response) => {
+    Axios.post(
+      "https://deco3801-betterlatethannever.uqcloud.net/items/count",
+      {}
+    ).then((response) => {
       if (response) {
         setCount(response.data.count[0].count);
-      } 
+      }
     });
   };
 
@@ -145,54 +177,114 @@ export default function MapScreen() {
       if (response.data.message) {
         errorAlert();
       } else if (response.data.success) {
-        validAlert()
-        console.log("worked")
+        validAlert();
+        console.log("worked");
       }
     });
   };
 
+  function getItem(itemType) {
+    if (itemType == 0) {
+      return require("../../assets/images/mask.jpg");
+    }
+
+    if (itemType == 1) {
+      return require("../../assets/images/gloves.png");
+    }
+
+    if (itemType == 2) {
+      return require("../../assets/images/bart.jpeg");
+    }
+  }
 
   // event that get asks for permission then gets the users inital location
   React.useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
         return;
       } else {
-        console.log('Permission to access locaion was approved')
+        console.log("Permission to access locaion was approved");
       }
 
       myFriends();
       let location = await Location.getCurrentPositionAsync({});
-      randomLocation.distance()
+      randomLocation.distance();
 
       // set Pin being used to change the pin
       setPin({
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
       });
     })();
-    }, []);
- 
+  }, []);
+
   return (
-    
     <View style={styles.container}>
-      <MapView style={styles.map}
-              // where the map will hover when opened Location is St Lucia
-              initialRegion = {{latitude: -27.4975,
-                                longitude: 153.0137,
-                                latitudeDelta: 0.005,
-                                longitudeDelta: 0.005}}
-                showsUserLocation = {true}
-                //method that will update the location of user when it changes
-                onUserLocationChange = {(e) => {
-                  //console.log("location changed", e.nativeEvent.coordinate);
-                  setPin({
-                    latitude: e.nativeEvent.coordinate.latitude,
-                    longitude: e.nativeEvent.coordinate.longitude,
-                  });
-                  /*
+      <MapView
+        style={styles.map}
+        // where the map will hover when opened Location is St Lucia
+        initialRegion={{
+          latitude: -27.4975,
+          longitude: 153.0137,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        showsUserLocation={true}
+        //method that will update the location of user when it changes
+        onUserLocationChange={(e) => {
+          // console.log("location changed", e.nativeEvent.coordinate);
+          setPin({
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+          });
+
+          if (
+            calcualteDistance(
+              initialCentre.latitude,
+              initialCentre.longitude,
+              e.nativeEvent.coordinate.latitude,
+              e.nativeEvent.coordinate.longitude
+            ) > 300
+          ) {
+            setInitialCentre({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+
+            const newItems = [];
+
+            for (var i = 0; i < items.length; i++) {
+              if (
+                calcualteDistance(
+                  items[i].latitude,
+                  items[i].longitude,
+                  e.nativeEvent.coordinate.latitude,
+                  e.nativeEvent.coordinate.longitude
+                ) < 500
+              ) {
+                newItems.push(items[i]);
+              }
+            }
+
+            if (newItems.length < 7) {
+              for (var x = newItems.length; x < 8; x++) {
+                const distance = Math.floor(Math.random() * 440) + 70;
+                const newLocation = randomCirclePoint(
+                  e.nativeEvent.coordinate,
+                  distance
+                );
+                newItems.push({
+                  itemType: Math.floor(Math.random() * UNIQUEITEMS),
+                  longitude: newLocation.longitude,
+                  latitude: newLocation.latitude,
+                });
+              }
+            }
+            setItems(newItems);
+          }
+          /*
                   countItems();
                   console.log("count:")
                   console.log(count);
@@ -213,85 +305,107 @@ export default function MapScreen() {
                   }
                   */
 
-                  UpdateLocation();
-                  setDistance({
-                    thing: calcualteDistance(pin.latitude, pin.longitude, uq.latitude, uq.longitude)
-                  });
-                }}
-                >
-          <Marker 
-            // marker that shows Birsbane and says Brisbane when hovered
-            coordinate = {{latitude: -27.470125,
-                          longitude: 153.021072,}}
-            pinColor = "blue"
-            >
-                <Callout>
-						      <Text>Brisbane</Text>
-					      </Callout>
-          </Marker>
-          <Marker 
-            // marker that shows UQ st lucia
-            coordinate = {{latitude: -27.4975,
-                          longitude: 153.0137,}}
-            pinColor = "#0000FF"
-            >
-                <Callout>
-						      <Text>UQ Campus</Text>
-					      </Callout>
-                
-          </Marker>
-          {
-              friends.map((friend, index) => {
-                  return (
-                      <Marker coordinate = {{latitude: friend.Latitude,
-                                             longitude: friend.Longitude,}}
-                              pinColor = {friend.InfectionStatus == 'Infected' ? 'red' : 'green'}>
-                      <Callout><Text>{friend.Username}</Text></Callout>
-                      </Marker>
-                  );
-              })
-          }
-
-          <Marker
-            //item marker for mask
-            coordinate = {{latitude: -27.496,
-                          longitude: 153.0137,}}
-            >
-              <Image
-              source={require("../../assets/images/mask.jpg")}
-              style={{ height: 50, width: 50 }}
-              />
-                <Callout>
-						      <Text>Mask</Text>
-					      </Callout>
-          </Marker>
-
-          <Marker
-              // Item marker for gloves
-            coordinate = {{latitude: -27.497,
-                          longitude: 153.012,}}
-            pinColor = "#00FF00"
-            >
-              <Image
-              source={require("../../assets/images/gloves.png")}
-              style={{ height: 50, width: 50 }}
-              />
-                <Callout>
-						      <Text>Gloves</Text>
-					      </Callout>
-          </Marker>
-
-          <Marker
-            // marker that shows the user location and is on top of the user icon from the MapView 
-            coordinate = {pin}
-            pinColor = "red"
+          UpdateLocation();
+          setDistance({
+            thing: calcualteDistance(
+              pin.latitude,
+              pin.longitude,
+              uq.latitude,
+              uq.longitude
+            ),
+          });
+        }}
+      >
+        <Marker
+          // marker that shows Birsbane and says Brisbane when hovered
+          coordinate={{ latitude: -27.470125, longitude: 153.021072 }}
+          pinColor="blue"
+        >
+          <Callout>
+            <Text>Brisbane</Text>
+          </Callout>
+        </Marker>
+        <Marker
+          // marker that shows UQ st lucia
+          coordinate={{ latitude: -27.4975, longitude: 153.0137 }}
+          pinColor="#0000FF"
+        >
+          <Callout>
+            <Text>UQ Campus</Text>
+          </Callout>
+        </Marker>
+        {friends.map((friend, index) => {
+          return (
+            <Marker
+              coordinate={{
+                latitude: friend.Latitude,
+                longitude: friend.Longitude,
+              }}
+              pinColor={friend.InfectionStatus == "Infected" ? "red" : "green"}
             >
               <Callout>
-						    <Text>User is {distance.thing} metres away</Text>
-					    </Callout>
-          </Marker>
-          <Circle //circle that is around the user, maybe can be used as the infection radius
-          center={pin} radius={100}/>
+                <Text>{friend.Username}</Text>
+              </Callout>
+            </Marker>
+          );
+        })}
+
+        {items.map((item) => {
+          return (
+            <Marker
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+            >
+              <Image
+                source={getItem(item.itemType)}
+                style={{ height: 50, width: 50 }}
+              />
+            </Marker>
+          );
+        })}
+
+        {/* <Marker
+          //item marker for mask
+          coordinate={{ latitude: -27.496, longitude: 153.0137 }}
+        >
+          <Image
+            source={require("../../assets/images/mask.jpg")}
+            style={{ height: 50, width: 50 }}
+          />
+          <Callout>
+            <Text>Mask</Text>
+          </Callout>
+        </Marker>
+
+        <Marker
+          // Item marker for gloves
+          coordinate={{ latitude: -27.497, longitude: 153.012 }}
+          pinColor="#00FF00"
+        >
+          <Image
+            source={require("../../assets/images/gloves.png")}
+            style={{ height: 50, width: 50 }}
+          />
+          <Callout>
+            <Text>Gloves</Text>
+          </Callout>
+        </Marker> */}
+
+        <Marker
+          // marker that shows the user location and is on top of the user icon from the MapView
+          coordinate={pin}
+          pinColor="red"
+        >
+          <Callout>
+            <Text>User is {distance.thing} metres away</Text>
+          </Callout>
+        </Marker>
+        <Circle //circle that is around the user, maybe can be used as the infection radius
+          center={pin}
+          radius={100}
+        />
       </MapView>
     </View>
   );
@@ -300,12 +414,12 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
